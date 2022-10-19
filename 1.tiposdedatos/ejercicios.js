af39bd5f28f3de5a1192be60777723f6265a99d3
zachary <tomklare20@gmail.com>
2022-10-18 22:42:54 -0500

    tarea de hoy

--- 1.tiposdedatos/ejercicios.js
+++ 1.tiposdedatos/ejercicios.js
@@ -1,1 +1,10 @@
-//averiguar el metodo de java scrip que me permite saber que tipo de datos tengo
+//averiguar el metodo de javascrip que me permite saber que tipo de datos tengo
+//El operador typeof devuelve una cadena que indica el tipo del operando sin evaluarlo. operando es la cadena, variable, palabra clave u objeto para el que se devolverá su tipo. Los paréntesis son opcionales.
+
+var miFuncion = new Function("5+2")
+var forma = "redonda"
+var tamano = 1
+var hoy = new Date()
+
+console.log(typeof(hoy))
+
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,1 +1,9 @@
 //averiguar la diferencia entre el var y let
+//var tiene la función o alcance global.
+//let deja tener el alcance del bloque.
+//var Se eleva a la parte superior de su alcance y se inicializa sin definir.
+//let También se elevó a la parte superior de su alcance, pero no se inicializó.
+//var Se puede actualizar o volver a declarar.
+//let Solo se puede actualizar y no se puede volver a declarar.
+//var Es una forma antigua de declarar una variable.
+//let 	Es una nueva forma de declarar variables introducida en ES6.
