11b03151b30ab224eaed58d5729dc9927926ebb7
zachary <tomklare20@gmail.com>
2022-10-20 10:06:19 -0500

    primer push

---
+++ 3.operadores/ejercicios.js
@@ -0,0 +1,13 @@
+//operador aritmetico
+let a = 12;
+let b = 10;
+let result = a+b;
+console.log(result)
+
+//operador logico
+console.log(true || false)
+
+//operador comparacionales
+console.log(2==4);
+
+//operador asignacion
---
+++ 3.operadores/index.js
@@ -0,0 +1,31 @@
+//conceptos sobre los operadores y tipos que existen en javascript
+
+//operador aritmetico
+//Con ellos podemos utilizar operadores aritméticos para realizar cálculos matemáticos en los operandos.
+//esta la suma (+), la multipilicar (*), la resta (-) y la division(/)
+
+//operadores logicos
+//Los operadores lógicos se usan para combinar dos valores Booleanos y devolver un resultado verdadero, falso o nulo. 
+//Los operadores lógicos también se denominan operadores Booleanos.
+//entre ellos estan los not (!) negacion, or (||) y and (&&)  
+
+//operadores comparacionales
+//Los operadores de comparación se utilizan para comparar operandos y devuelven un valor lógico (verdadero o falso) 
+//sobre la base de la comparación
+//== / igual
+//!= / no igual
+//=== / igual tanto el dato como valor
+//< / menor que
+//<= / menor o igual que
+
+//operadores asignacion
+//controla el flujo de la programacion para el aumento o disminucion de la variable.
+// (valor)++ aumento en unidad, (valor)-- disminucion en unidad, (valor)/=3 hara que divida el valor entre 3
+
+//operador de string
+//se utiliza para poder concatenar cadenas de texto (string == +)
+
+//operador relacionales
+//Un operador relacional compara sus operandos y devuelve un valor Boolean basado en si la comparación es verdadera.
+//in, 
+
